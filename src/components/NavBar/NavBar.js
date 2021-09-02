47fe6e442187b557d100bc84790b3f5a5f1495f3
Mariam Viera <mariamviera7@gmail.com>
2021-09-01 22:45:09 -0300

    nav bar

---
+++ src/components/NavBar/NavBar.js
@@ -0,0 +1,21 @@
+import './NavBar.css'
+
+const NavBar = () => {
+    return(
+    <nav className="NavBar">
+        <div className="LeftNav">
+            <div className="LeftNavigation">
+                <button className="Option">Home</button>
+                <button className="Option">About</button>
+            </div>
+        </div>
+        <div className="RightNav">
+            <div className="RightNavigation">
+                <button className="Option">Log In</button>
+            </div>
+        </div>
+    </nav>
+    )
+}
+
+export default NavBar
